d96b5de826b74a3c0ebe4a5d541a0b35e6ea7934
MillennaLeticia <veglust.millenna@gmail.com>
2025-08-31 11:27:10 -0400

    Add files via upload

---
+++ portifolio.js
@@ -0,0 +1,16 @@
+const portfolioData = {
+    nome: "Seu Nome",
+    profissao: "Desenvolvedor Front-End",
+    sobre: "Sou um desenvolvedor apaixonado por criar experiências digitais incríveis.",
+    tecnologias: [
+        { nome: "HTML5", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
+        { nome: "CSS3", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
+        { nome: "JavaScript", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
+        { nome: "React", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" }
+    ],
+    projetos: [
+        { nome: "Pokédex", descricao: "Aplicativo de Pokédex usando API REST", link: "#" },
+        { nome: "Jogo de Cartas Yu-Gi-Oh!", descricao: "Jogo interativo de cartas com flip e stats", link: "#" },
+        { nome: "Portfólio Dinâmico", descricao: "Meu portfólio dinâmico carregando dados via JSON", link: "#" }
+    ]
+};
